d705c0e1b73baa33849adc23f3d744e391cbc887
Deven Bansod <devenbansod@users.noreply.github.com>
2015-04-03 22:26:41 +0530

    Added getELSheet method required while printing

--- main/java/bits/arcd/model/GraduationRequirements.java
+++ main/java/bits/arcd/model/GraduationRequirements.java
@@ -78,6 +78,10 @@ public class GraduationRequirements {
 		System.out.println();
 	}
 	
+	public EligibilitySheetQueries getELSheet() {
+		return this.elSheet;
+	}
+	
 	private void loopThroughSemesters(EligibilitySheetQueries elsheet){
 
 		for(Semester s: elsheet.getChart().getSemsInChart()) {
